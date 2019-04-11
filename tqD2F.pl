ef8b1f0dbb782127b28d093d2264dbaa42165453
Atsushi Sakai <SAKAI.Atsushi@nims.go.jp>
2019-04-11 14:07:46 +0900

    update 20190411-14:08:30

--- tqD2F.pl
+++ tqD2F.pl
@@ -1,4 +1,4 @@
-#!/bin/env perl
+#!/usr/bin/env perl
 $file = $ARGV[0];
 $head = "";
 %rls = ();
--- tqR2D.pl
+++ tqR2D.pl
@@ -1,4 +1,4 @@
-#!/bin/env perl
+#!/usr/bin/env perl
 $file = $ARGV[0];
 $head = "";
 %rls = ();
@@ -36,21 +36,94 @@ if( "$rls{'$X$Application'}" eq '$X$Mathematica' ){
 	print DATA "cat ".$fedfile." | "."math";
 	close(DATA);
 	# create metacomannd
-	$meta = '$M$./tq.o ($-FW,'.$copt.')';
+	$meta '$M$./tq.o ($-FW,',$copt.')';
 	print $meta;
 	print "\n";
 	print '$X$Get($`~/gitsrc/MATH_SCRIPT/SCRIPTS/DataFederation.m)';
 	print "\n";
 }elsif( "$rls{'$X$Application'}" eq '$X$Python' && "$rls{'$X$DDF'}" eq '$X$PackedData'){
-	$fedfile = $file.".dry.fed";
+#	$fedfile = $file.".dry.fed";
 	$comfile = $file.".dry.fed.com";
 	$target = $rls{'$X$Target'};
-	if(length($rls{'$X$FederationFile'}) > 0){
-		$fedfile = $rls{'$X$FederationFile'};
-	}
+#        if(length($rls{'$X$FederationFile'}) > 0){
+#		$fed1file = $rls{'$X$FederationFile'};
+#		$fed1file =~ s/^\$`//;
+#	}
 	$target =~ s/^\$`//;
+        $fedfile = $rls{'$X$FederationFile'};
+	$fedfile =~ s/^\$`//;
+        if(length($rls{'$X$B_FederationFile'}) > 0){
+		$bfedfile = $rls{'$X$B_FederationFile'};
+		$bfedfile =~ s/^\$`//;
+	}else {
+		$bfedfile = "";
+	}        
+	if(length($rls{'$X$A_FederationFile'}) > 0){
+		$afedfile = $rls{'$X$A_FederationFile'};
+		$afedfile =~ s/^\$`//;
+	} else {
+		$afedfile = "";
+	}
 	open(DATA,">",$comfile);
-	print DATA "python"." $fedfile"," $target";
+	print DATA "$bfedfile\n";
+	print DATA "python"," $fedfile"," $target\n";
+	print DATA "$afedfile\n";
+	close(DATA);
+}elsif( "$rls{'$X$Application'}" eq '$X$ruby' && "$rls{'$X$DDF'}" eq '$X$PackedData'){
+#	$fedfile = $file.".dry.fed";
+	$comfile = $file.".dry.fed.com";
+	$target = $rls{'$X$Target'};
+#        if(length($rls{'$X$FederationFile'}) > 0){
+#		$fed1file = $rls{'$X$FederationFile'};
+#		$fed1file =~ s/^\$`//;
+#	}
+	$target =~ s/^\$`//;
+        $fedfile = $rls{'$X$FederationFile'};
+	$fedfile =~ s/^\$`//;
+        if(length($rls{'$X$B_FederationFile'}) > 0){
+		$bfedfile = $rls{'$X$B_FederationFile'};
+		$bfedfile =~ s/^\$`//;
+	}else {
+		$bfedfile = "";
+	}        
+	if(length($rls{'$X$A_FederationFile'}) > 0){
+		$afedfile = $rls{'$X$A_FederationFile'};
+		$afedfile =~ s/^\$`//;
+	} else {
+		$afedfile = "";
+	}
+	open(DATA,">",$comfile);
+	print DATA "$bfedfile\n";
+	print DATA "ruby"," $fedfile"," $target\n";
+	print DATA "$afedfile\n";
+	close(DATA);
+}elsif( "$rls{'$X$Application'}" eq '$X$cmd' && "$rls{'$X$DDF'}" eq '$X$PackedData'){
+#	$fedfile = $file.".dry.fed";
+	$comfile = $file.".dry.fed.com";
+	$target = $rls{'$X$Target'};
+	$target =~ s/^\$`//;
+        $fedfile = $rls{'$X$FederationFile'};
+	$fedfile =~ s/^\$`//;
+        if(length($rls{'$X$B_FederationFile'}) > 0){
+		$bfedfile = $rls{'$X$B_FederationFile'};
+		$bfedfile =~ s/^\$`//;
+	}else {
+		$bfedfile = "";
+	}        
+	if(length($rls{'$X$A_FederationFile'}) > 0){
+		$afedfile = $rls{'$X$A_FederationFile'};
+		$afedfile =~ s/^\$`//;
+	}        
+	if(length($rls{'$X$A_FederationFile'}) > 0){
+		$afedfile = $rls{'$X$A_FederationFile'};
+		$afedfile =~ s/^\$`//;
+	} else {
+		$afedfile = "";
+	}
+	open(DATA,">",$comfile);
+	print DATA "$bfedfile\n";
+	print DATA "sh"," $fedfile\n";
+	print DATA "$afedfile\n";
 	close(DATA);
 }
 
